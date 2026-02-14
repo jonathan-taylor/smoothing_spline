caee428a247ffe21d42fe81aa8e5e91194bcbf77
Jonathan Taylor <jonathan.taylor@stanford.edu>
2026-02-14 14:40:16 -0800

    have pybind11 not fail silently

--- setup.py
+++ setup.py
@@ -2,30 +2,17 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 import sys
 import setuptools
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-# Use pybind11's helpers
-# We check for pybind11. If not present, we can't build the extension.
-try:
-    from pybind11.setup_helpers import Pybind11Extension, build_ext
-    has_pybind11 = True
-except ImportError:
-    has_pybind11 = False
-
-if has_pybind11:
-    ext_modules = [
-        Pybind11Extension(
-            "smoothing_spline._spline_extension",
-            ["src/spline_basis.cpp"],
-            # Example: passing in the version
-            define_macros = [('VERSION_INFO', '"0.0.1"')],
-            include_dirs = ["src/eigen"],
-        ),
-    ]
-else:
-    # If pybind11 is not installed, we can't build the extension.
-    # We might want to just skip it or error out.
-    # For this exercise, we assume pybind11 is present as we checked it.
-    ext_modules = []
+ext_modules = [
+    Pybind11Extension(
+        "smoothing_spline._spline_extension",
+        ["src/spline_basis.cpp"],
+        # Example: passing in the version
+        define_macros = [('VERSION_INFO', '"0.0.1"')],
+        include_dirs = ["src/eigen"],
+    ),
+]
 
 setup(
     name="smoothing_spline",
@@ -36,5 +23,5 @@ setup(
     # adding a setup.py allows us to define extensions.
     packages=find_packages(),
     ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext} if has_pybind11 else {},
+    cmdclass={"build_ext": build_ext},
 )
